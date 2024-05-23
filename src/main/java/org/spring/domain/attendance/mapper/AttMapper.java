515d2a235848c5f6a382d2d03b7e7408ad75db89
DoSangWoo <k24u16@gmail.com>
2024-05-23 19:49:30 +0900

    일용직 근태관리 기능 추가

--- src/main/java/org/spring/domain/attendance/mapper/AttMapper.java
+++ src/main/java/org/spring/domain/attendance/mapper/AttMapper.java
@@ -1,11 +1,18 @@
 package org.spring.domain.attendance.mapper;
 
 import org.spring.domain.attendance.model.AttendanceEntity;
+import org.spring.domain.attendance.model.AttendanceGroupEntity;
+import org.spring.domain.employee.model.EmployeeEntity;
 
 import java.util.List;
 
 public interface AttMapper {
 
     List<AttendanceEntity> getEmployeeAttendance();
+
+    // 일용직 리스트
+    List<EmployeeEntity> getDayWorkerList(String srchKwrd);
+
+	List<AttendanceGroupEntity> selectAttendanceGroup();
 
 }
